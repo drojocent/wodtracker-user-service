3fe8aff5beb1c16c5762fa8a651f0320153d0e51
Daniel Rojo <drojocent@uoc.edu>
2026-04-16 20:03:02 +0200

    create

---
+++ src/main/java/com/wodtracker/userservice/WodtrackerUserServiceApplication.java
@@ -0,0 +1,12 @@
+package com.wodtracker.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WodtrackerUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WodtrackerUserServiceApplication.class, args);
+    }
+}
