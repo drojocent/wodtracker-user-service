2137c7c93ec1d3fbf92f15ffc1d76516fd6da75b
Daniel Rojo <drojocent@uoc.edu>
2026-04-19 15:12:28 +0200

    add CI pipeline

---
+++ src/test/java/com/wodtracker/userservice/WodtrackerUserServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.wodtracker.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WodtrackerUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
